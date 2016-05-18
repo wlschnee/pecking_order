d77dc6dff20825d8c27a9f58b3be8cc62956856a
wlschnee <will@willschneekloth.com>
2016-05-18 16:12:09 -0400

    added event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,16 @@
 class EventsController < ApplicationController
-  
+  before_action :select_event, only: [:show, :update, :delete]
+
   def new
     @event = Event.new
   end
 
   def create
     @event = Event.create(event_params)
+    @event.start_time = parse_time
+    @event.host = @current_user
+    @event.save
+    binding.pry
     redirect_to @event
   end
 
@@ -15,9 +20,15 @@ class EventsController < ApplicationController
 
   private
     def event_params
-    params.require(:event).permit(:location_id,:start_time,:meeting_place,:duration,:host_id)
+      params.require(:event).permit(:name, :location_id, :meeting_place,:duration)
     end
 
+    def parse_time
+      time = params[:event]
+      DateTime.new(time["start_time(1i)"].to_i,time["start_time(2i)"].to_i,time["start_time(3i)"].to_i,time["start_time(4i)"].to_i,time["start_time(5i)"].to_i)
+    end
 
-
+    def select_event
+      @event = Event.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517214542) do
+ActiveRecord::Schema.define(version: 20160518191821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160517214542) do
     t.integer  "host_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "name"
   end
 
   create_table "locations", force: :cascade do |t|
