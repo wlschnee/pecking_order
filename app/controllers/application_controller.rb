185a3208f194c8acc6ebaa1a6c1942071590064f
Lamine Ndaw <brahimndaw@gmail.com>
2016-05-19 10:30:42 -0400

    Logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+  helper_method :current_user, 
 
 private 
+
+def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
 def require_login
   unless session[:user_id] != nil
     redirect_to login_path
   end
 end
+
+
 
 end
