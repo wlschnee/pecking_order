ca2d903f98a5e95144a5966cea2753c0e331c455
JustinNazari <justinnazari@gmail.com>
2016-05-18 20:45:27 -0400

    fixed minor boo boo in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ end
 def create
   @user = User.find_by(email: params[:email])
   if @user != nil && @user.authenticate(params[:password]) 
-  session[:flash] = "Logged in as #{@user.full_name}"
   session[:user_id] = @user.id
   session[:flash] = "Logged in as #{@user.full_name}"
   redirect_to events_path
