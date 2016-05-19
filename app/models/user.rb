928d630d498f148a349afc4089b23187557931eb
JustinNazari <justinnazari@gmail.com>
2016-05-19 17:07:56 -0400

    rendered upcoming events on user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :events, foreign_key: :host_id
+  has_many :registrations, foreign_key: :guest_id
   has_many :comments
   has_many :tips
   has_secure_password
@@ -9,6 +10,22 @@ class User < ActiveRecord::Base
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def upcoming_events
+    @all_events = []
+    @something = self.events
+    @something.each do |something|
+      @all_events << something
+    end
+    @all_registrations = self.registrations
+    @all_registrations.each do |registration|
+      @all_events << registration.event
+    end
+    @upcoming = @all_events.select do |event|
+      event.start_time > Time.now
+    end
+    @upcoming
   end
 
 end
