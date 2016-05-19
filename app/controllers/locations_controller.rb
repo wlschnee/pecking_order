baf47a953eac5d452c0932454b51308fc9fdbf93
wlschnee <will@willschneekloth.com>
2016-05-19 18:12:17 -0400

    cleaned up locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,5 @@
 class LocationsController < ApplicationController
 
-  def index
-  @locations = Location.all
-  respond_to do |format|
-    format.html  
-    format.json {@location = Location.search(params[:term])}
-  end
-
   def show
   @location = Location.find(params[:id])
   end
