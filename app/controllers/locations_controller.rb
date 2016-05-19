f4443c9cb46587ab0e165bf8b2426dad8ae72148
kdonovan2005 <kdonovan2005@gmail.com>
2016-05-19 17:54:29 -0400

    added the end

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,11 @@
 class LocationsController < ApplicationController
 
   def index
-  @locations = Location.all
-  respond_to do |format|
-    format.html  
-    format.json {@location = Location.search(params[:term])}
+    @locations = Location.all
+    respond_to do |format|
+      format.html
+      format.json {@location = Location.search(params[:term])}
+    end
   end
 
   def show
