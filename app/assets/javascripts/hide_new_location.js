45dd288a4a9d35be5f5e5ac165f202760ac9cfc9
Will Schneekloth <wlschnee@users.noreply.github.com>
2016-05-20 11:57:31 -0400

    Update hide_new_location.js

--- app/assets/javascripts/hide_new_location.js
+++ app/assets/javascripts/hide_new_location.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  debugger
     $("#event_location_id").change(function(){
       if ($("#event_location_id option:selected").text() != "Make New Location"){
         $(".new_location_form_elements").hide();
