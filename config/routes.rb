901e13249e16babe25002a216558521a8264543b
Lamine Ndaw <brahimndaw@gmail.com>
2016-05-21 18:43:19 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
     member do 
       post 'like'
     end
+  end
   resources :events
   resources :users
   resources :registrations
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
