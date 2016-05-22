993ff1e16fe26186c5c7ec08bf64f4578839121e
Lamine Ndaw <brahimndaw@gmail.com>
2016-05-22 15:51:09 -0400

    Befriend a user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,10 +4,10 @@ class FriendshipsController < ApplicationController
   def create
   @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
   if @friendship.save
-    flash[:notice] = "Added friend."
+    flash[:success] = "Added friend."
    redirect_to current_user
   else
-    flash[:notice] = "Unable to add friend."
+    flash[:danger] = "You have already befriended this user."
     redirect_to users_path
   end
   end
@@ -16,7 +16,7 @@ class FriendshipsController < ApplicationController
   def destroy
   @friendship = current_user.friendships.find(params[:id])
   @friendship.destroy
-  flash[:notice] = "Removed friendship."
+  flash[:danger] = "Friendship removed."
   redirect_to current_user
   end
 
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => 'User'
+  validates_uniqueness_of :user, scope: :friend
 end
