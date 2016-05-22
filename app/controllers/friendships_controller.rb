fceb717cb0fe8720eaddec8b35bbfdb8728a0565
Lamine Ndaw <brahimndaw@gmail.com>
2016-05-22 16:58:02 -0400

    User can access his friends profile through his page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,15 +3,18 @@ class FriendshipsController < ApplicationController
 
   def create
   @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-  if @friendship.save
-    flash[:success] = "Added friend."
-   redirect_to current_user
-  else
-    flash[:danger] = "You have already befriended this user."
-    redirect_to users_path
-  end
+    if @friendship.save
+      flash[:success] = "Added friend."
+     redirect_to current_user
+    else
+      flash[:danger] = "You have already befriended this user."
+      redirect_to users_path
+    end
   end
 
+  def show
+  @user = User.find(params[:id])
+  end
 
   def destroy
   @friendship = current_user.friendships.find(params[:id])
