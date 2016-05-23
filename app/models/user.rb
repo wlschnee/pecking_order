f735ef573b1fd6c50414611da2e634a4b9c2c929
Lamine Ndaw <brahimndaw@gmail.com>
2016-05-22 20:00:02 -0400

    Removed the inverse relationship in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
   has_many :tips
   has_many :friendships
   has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
   validates :email, presence: true, uniqueness: true, allow_nil: true
