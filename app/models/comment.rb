6e7b74f86f01fc0944d57844d15e85d2989b4f4d
kdonovan2005 <kdonovan2005@gmail.com>
2016-05-24 16:33:49 -0400

    rails magic for timestamps

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,23 +1,25 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  include ActionView::Helpers::DateHelper
 
   def timestamp
-    time_elapsed = (Time.now - self.created_at.to_time)/1.minute
-    if time_elapsed < 60 && time_elapsed == 1
-      "Commented #{time_elapsed.round} minute ago"
-    elsif time_elapsed < 60
-      "Commented #{time_elapsed.round} minutes ago"
-    elsif time_elapsed >= 60
-      time_elapsed = (Time.now - self.created_at.to_time)/1.hour
-      if time_elapsed == 1
-        "Commented #{time_elapsed.round} hour ago"
-      else
-        "Commented #{time_elapsed.round} hours ago"
-      end
-    else
-      @comment.created_at.strftime("%a, %b %-d, %Y, %I:%M %p")
-    end
+    time_ago_in_words(self.created_at) + ' ago'
+    # time_elapsed = (Time.now - self.created_at.to_time)/1.minute
+    # if time_elapsed < 60 && time_elapsed == 1
+    #   "Commented #{time_elapsed.round} minute ago"
+    # elsif time_elapsed < 60
+    #   "Commented #{time_elapsed.round} minutes ago"
+    # elsif time_elapsed >= 60
+    #   time_elapsed = (Time.now - self.created_at.to_time)/1.hour
+    #   if time_elapsed == 1
+    #     "Commented #{time_elapsed.round} hour ago"
+    #   else
+    #     "Commented #{time_elapsed.round} hours ago"
+    #   end
+    # else
+    #   @comment.created_at.strftime("%a, %b %-d, %Y, %I:%M %p")
+    # end
   end
 
 end
