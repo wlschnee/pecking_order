b17c7dfaf4e41ed8ef7b6b45cc3b0ef9359a38d4
kdonovan2005 <kdonovan2005@gmail.com>
2016-05-24 19:29:04 -0400

    fixed image

--- db/migrate/20160524232343_online_hosted_fault_image.rb
+++ db/migrate/20160524232343_online_hosted_fault_image.rb
@@ -1,5 +1,5 @@
 class OnlineHostedFaultImage < ActiveRecord::Migration[5.0]
   def change
-    change_column_default(:users, :picture, "/assets/default_profile.png")
+    change_column_default(:users, :picture, "http://i.imgur.com/VVc1lle.png")
   end
 end
