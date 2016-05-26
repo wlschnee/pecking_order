c9297d5c8fabef3a01e3dfaca1c8e4dc51ba507e
wlschnee <will@willschneekloth.com>
2016-05-26 11:22:33 -0400

    added invite friends form to event show page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, :subject => "You've been invited to #{event.name} by #{sender.first_name}!")
   end
 
+  def invite_to_service(user, sender, event)
+    @user = user
+    @sender = sender
+    @event = event
+    mail(to: @user[:email], :subject => "Invitation to join Social Eats from #{sender.first_name}!")
+  end
+
 end
