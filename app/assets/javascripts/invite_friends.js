f6c01e0e955f52e916f25fc5d2d2c718564ce6e7
kdonovan2005 <kdonovan2005@gmail.com>
2016-05-26 13:55:09 -0400

    fixed email sending invites?

--- app/assets/javascripts/invite_friends.js
+++ app/assets/javascripts/invite_friends.js
@@ -1,5 +1,5 @@
+$('#event_for_email').val(window.location.pathname);
 $("#invitation_send").on('click', function(e) {
-    $('#event_for_email').val(window.location.pathname);
     $(this).submit();
     $('#invitation_name').val("");
     $('#invitation_email').val("");
