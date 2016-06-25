478f4a1da9a94b77b2854d59ad36ee7c470b64fc
Lamine Ndaw <brahimndaw@gmail.com>
2016-06-25 00:02:53 -0400

    pulling in master

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,7 @@ class LocationsController < ApplicationController
 
   def index
     @locations = Location.all.sort_by{|likes| likes.likes_message(current_user)}.reverse
-    respond_to do |format|
-      format.html
-      format.json { render json: @locations.to_json}
+
     end
   end
 
